70aefbe48d190dd44ff6bedc06b3fb47f98a0a18
hippoandy <hippo1993928@gmail.com>
2019-02-12 20:32:07 -0300

    Add comments

--- utilsDAWS/ops_data/data.py
+++ utilsDAWS/ops_data/data.py
@@ -18,6 +18,12 @@ __all__ = [
     'list_deduplicated',
 ]
 
+''' README
+
+Test if the given API link is accessible.
+
+Retrun: boolean
+'''
 def is_api_success( r ):
     if( r != None and r.status_code == 200 ): return True
     else: return False
@@ -26,11 +32,12 @@ def api_json_available( r ):
     try: return not empty_struct( r.json() )
     except: return False
 
-# find country domain in url
 ''' README
 
-Return value format: two digit country domain
-ex. 'ar', 'bo', 'cl'
+Find country domain in the given URL.
+
+Return: two digit country domain
+  - ex. 'ar', 'bo', 'cl'
 '''
 def url_country_domain( url ):
     try:    domain = re.findall( r"\b" + r'\.[a-z]{2}\/' + r"\b", str(url) )[ 0 ]
@@ -38,63 +45,171 @@ def url_country_domain( url ):
     domain = clear_char( domain, "./" )
     return domain
 
-# find the numeric value within a string
+''' README
+
+Find the numeric value within a string.
+
+Return: the numeric value founded
+'''
 def find_numeric( text ):
     if( text == invalid_val() ): return invalid_val()
     try:    return re.findall( r'\d+', text )[ 0 ]
     # try:    return re.search( r'\d+', text ).group( 0 )
     except: return invalid_val()
 
+''' README
+
+Find all the numeric values within a string.
+
+Return: list of the numeric values founded
+'''
 def find_all_numeric( text ):
     if( text == invalid_val() ): return invalid_val()
     try:    return re.findall( r'\d+', text )
     # try:    return re.search( r'\d+', exp ).group( 0 )
     except: return []
 
-# get the unit associate with a number
+''' README
+
+Get the unit associate with a number.
+
+Input: a numberassociate with a string, for example: '1 liter'
+
+Return: the text been found
+  - for example:
+      input:    '1 liter'
+      return:   'liter'
+'''
 def get_unit( text ):
-    try:    return re.sub( r'\d+', ' ', text ).replace( ' ', '' )
+    try:    return re.sub( r'\d+', ' ', text ).lstrip( ' ' )
     except: return invalid_val()
 
-# check if struct is_empty
+''' README
+
+Check if the given struct (list, dict) is empty.
+
+Return: boolean value
+'''
 def empty_struct( struct ):
     if struct: return False
     else: return True
 
+''' README
+
+Universal value for unavailable data
+
+Return: 'N/A' in str type
+'''
 def invalid_val(): return 'N/A'
 
-# the basic cleaning that applies to all the value
+''' README
+
+The basic cleaning that applies to the given text.
+
+The following char will be cleared by this funct.:
+  - '\n': new line
+  - '\t': tab
+  - '\r'
+  - ',': comma
+
+Return: text in str type
+'''
 def basic_cleaning( text ):
     return clean_str( clear_comma( text ) )
 
-# make sure there is no special char in a value
+''' README
+
+Clear several special chars in the given text.
+
+The following char will be cleared by this funct.:
+  - '\n': new line
+  - '\t': tab
+  - '\r'
+
+Return: text in str type
+'''
 def clean_str( text ):
     return str(text).replace( '\n', '' ).replace( '\r', '' ).replace( '\t', '' )
 
-# remove ',' and '.' from numeric values
+''' README
+
+Clear several chars in the given text (numeric).
+
+The following char will be cleared by this funct.:
+  - ',': comma
+  - '.': dot
+
+Return: text in str type
+'''
 def clean_num( text ):
     return clear_dot( clear_comma( text ) )
 
+''' README
+
+Clear all the sub-string in the given list within a given text.
+
+Input:
+  - text to be operated
+  - list of sub-string to be removed
+
+Return: text in str type
+'''
 def clear_multi_text( text, l=[] ):
     for e in l: text = text.replace( e, '' )
     return text
 
-# clear the char in the string tar
+''' README
+
+Clear all the chars in the given string of chars within a given text.
+
+Input:
+  - text to be operated
+  - char string:
+      for example, to remove ',', '.', and '|', input: ',.|'
+
+Return: text in str type
+'''
 def clear_char( text, tar ):
     for t in tar: text = text.replace( t, '' )
     return text
 
+''' README
+
+Clear '.' (dot) in the given text.
+
+Return: text in str type
+'''
 def clear_dot( text ):
     return str(text).replace( '.', '' )
 
-# clear ',' in the value
+''' README
+
+Clear ',' (comma) in the given text.
+
+Return: text in str type
+'''
 def clear_comma( text ):
     return str(text).replace( ',', '' )
 
-# replace ',' with '-'
+''' README
+
+Replace ',' (comma) with '-' (hyphen)
+
+Return: text in str type
+'''
 def comma_to_hyphen( text ):
     return str(text).replace( ',', '-' )
 
+''' README
+
+Check if a given value is a numeric one
+
+Input:
+  - text to be transform
+  - type: [ 'int', 'float' ]
+
+Return: boolean value
+'''
 def is_numeric( val, type='float' ):
     val = str(val) # make sure it is not 'NoneType'
     try: # make sure the value is numeric
@@ -103,18 +218,50 @@ def is_numeric( val, type='float' ):
         return True
     except: return False
 
-# make sure the value is numeric, otherwise return invalid_val()
+''' README
+
+Make sure the value is numeric, otherwise return invalid_val().
+
+Input:
+  - text to be transform
+  - type: [ 'int', 'float' ]
+
+Return:
+  - numeric value in str type
+  - invalid_val()
+'''
 def numeric( val, type='float' ):
     if( is_numeric( val, type ) ): return str(val)
     else: return invalid_val()
 
-# create numerical value
+''' README
+
+Create numerical value. If failed, return invalid_val().
+
+Input:
+  - text to be transform
+  - type: [ 'int', 'float' ]
+
+Return:
+  - numeric value in int/float type
+  - invalid_val()
+'''
 def make_numeric( text, type='float' ):
     if( text != invalid_val() ):
         if( type == 'int' ): return int(text)
         else: return float(text)
     else: return invalid_val()
 
+''' README
+
+Delete duplicated entries within a given list.
+
+Lib. dependency: pandas
+
+Input: list of items
+
+Return: list of items (deduplicated)
+'''
 def list_deduplicated( list_ ):
     df = pd.DataFrame.from_records( { 'item': list_ } )
     df = df.drop_duplicates()
