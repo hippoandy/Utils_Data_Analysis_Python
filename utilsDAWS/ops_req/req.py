1409b65f1663b1d23a96cc9b482dd3188b7680bb
hippoandy <hippo1993928@gmail.com>
2019-02-12 19:17:54 -0300

    New funct. category

---
+++ utilsDAWS/ops_req/req.py
@@ -0,0 +1,30 @@
+import requests
+import textwrap
+
+__all__ = [
+    'send_req',
+]
+
+# send a url request
+def send_req( url, timeout=10, err_msg="" ):
+    try:
+        r = requests.get( url, timeout=timeout ) # get web code
+    except Exception as e:
+        # print( textwrap.dedent( f'''
+        #     Error while accessing the url: {url}
+        #         Error Msg: {repr(e)}
+        #         Trace: {format_exc()}
+        # '''))
+        print( textwrap.dedent( f'''
+            Error while accessing the url: {url}
+                Error Msg: {repr(e)}
+        '''))
+        return None
+    # if( r.status_code != 200 ):
+    #     print( textwrap.dedent( f'''
+    #         Unable to access the url: {url}
+    #             Status Code: {r.status_code}
+    #             Error Msg: {err_msg}
+    #     '''))
+    #     return r
+    return r
