f6e0883003ca5b9fa1842163e7761621ac3eeebf
hippoandy <ycaho@ucdavis.edu>
2019-02-12 17:04:54 -0300

    General paramenters

---
+++ config.py
@@ -0,0 +1,16 @@
+
+
+# general file path ---------------------------------------
+path_data = './data/'
+# --------------------------------------- general file path
+
+# general settings ----------------------------------------
+start = 0
+concurrent = 20
+partition = 20
+timeout = 10
+
+sleep_short = 1
+sleep_med = 3
+sleep_long = 10
+# ---------------------------------------- general settings
