398aa6c40e2774f6463529937c5178c35bd9cbcf
hippoandy <hippo1993928@gmail.com>
2019-02-12 19:16:19 -0300

    Add new funct category

--- utilsDAWS/__init__.py
+++ utilsDAWS/__init__.py
@@ -2,7 +2,8 @@ from .ops_data import *
 from .ops_thread import *
 from .ops_file import *
 from .ops_log import *
+from .ops_req import *
 
 __all__ = [
-    'ops_data', 'ops_thread', 'ops_file', 'ops_log'
+    'ops_data', 'ops_thread', 'ops_file', 'ops_log', 'ops_req'
 ]
