4917b4b9ecf919695c6d947e213931eaa4e67a23
hippoandy <ycaho@ucdavis.edu>
2019-02-12 17:05:03 -0300

    setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name='utils_da_py',
+    version='0.0.1',
+    description='This package has shared classes and functions for data analysis and scraping purposes.',
+    author='Yu-Chang Ho (Andy)',
+    author_email='ycaho@ucdavis.edu',
+    packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
+    # license='LICENSE.txt',
+    long_description=open('README.md').read()
+)
