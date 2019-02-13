c64a7cfa14b40c51a6326dcff25d8b3cee9449ed
hippoandy <hippo1993928@gmail.com>
2019-02-12 23:42:13 -0300

    Add check parent folder exists funct

--- utilsDAWS/ops_file/folder.py
+++ utilsDAWS/ops_file/folder.py
@@ -1,9 +1,25 @@
 import os
 
-__all__ = [ 'mkdir_p' ]
+__all__ = [ 'mkdir_p', 'is_parent_dir_exist' ]
 
-# check if parent dir exist
+''' README
+
+Equivalent to UNIX command: '$ mkdir -p'
+
+Return: Nil
+'''
 def mkdir_p( path ):
-    path_parent = os.path.dirname( path )
-    if( not os.path.exists( path_parent ) ):
+    if( not is_parent_dir_exist( path ) ):
+        path_parent = os.path.dirname( path )
         os.makedirs( path_parent, exist_ok=True )
+
+''' README
+
+Check if parent dir exist.
+
+Return: boolean value
+'''
+def is_parent_dir_exist( path ):
+    path_parent = os.path.dirname( path )
+    if( not os.path.exists( path_parent ) and os.path.isdir( path_parent ) ): return False
+    else: return True
