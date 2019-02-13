a42da15e4a249f52e7d12a8fa9678eae27514942
hippoandy <ycaho@ucdavs.edu>
2019-02-13 11:05:22 -0300

    Add file deletion funct

--- utilsDAWS/ops_file/file.py
+++ utilsDAWS/ops_file/file.py
@@ -1,8 +1,10 @@
 from pathlib import Path
+import os
 
 __all__ = [
     'is_file_exist',
-    'check_file_exist'
+    'check_file_exist',
+    'rm_file'
 ]
 
 def check_file_exist( path ):
@@ -14,3 +16,6 @@ def is_file_exist( path ):
     f = Path( path )
     if( f.is_file() ): return True
     else: return False
+
+def rm_file( fpath ):
+    if( os.path.isfile( fpath ) ): os.remove( fpath )
