d977a10a45945c90f5f64b0290f823148fe00965
hippoandy <ycaho@ucdavs.edu>
2019-02-13 11:04:57 -0300

    Add new global var

--- utilsDAWS/config.py
+++ utilsDAWS/config.py
@@ -7,7 +7,13 @@ f_data_csv = r'*.csv'
 f_concated_json = r'concated.json'
 f_concated_csv = r'concated.csv'
 f_merged_csv = r'merged.csv'
+f_log_sc_json = r'*_err.json'
+f_result_clean_log_json = r'to-retry.json'
 # --------------------------------------- general file path
+
+# general task name ---------------------------------------
+n_scrape_clean = 'clear-log'
+# --------------------------------------- general task name
 
 # general settings ----------------------------------------
 encoding_f = 'utf-8'
