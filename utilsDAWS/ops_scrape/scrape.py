0be02a7c2769846c78dd79fd24d05c40df487c9e
hippoandy <ycaho@ucdavis.edu>
2019-02-14 20:33:03 -0300

    Clean code and update logic

--- utilsDAWS/ops_scrape/scrape.py
+++ utilsDAWS/ops_scrape/scrape.py
@@ -22,61 +22,52 @@ from traceback import format_exc
 class scraper():
     ''' multi-threading scraper '''
     def __init__( self, name='scrape', storage=config.path_data,\
-        parse_func=(lambda x: x.text),\
+        parse_funct=(lambda x: x.text),\
         concurrent=config.concurrent, timeout=config.timeout ):
-        '''
-            name: job name, prefix for all data files
-            list_job: all urls to scrape
-            list_tmp: temporarily holds response object for urls in list_job
-            parse_func: parsing function, provided by user
 
-            storage: storage directory for all data
-            path_f_data: path for data
-            path_f_scrape_err: path for scrape errors
-            path_f_parse_err: path for parse errors
+        self.name = name                # job name, prefix for all data files
+        self.parse_funct = parse_funct  # parsing function
 
-            job_queue: thread-safe synchronized queue
-            lock: Lock for synchronized variable read/write
-            concurrent: number of threads
-            timeout: timeout for each request
-            finished: tracking scraping progress
-
-            list_data: final data, after scraping, all data should be here
-            list_scrape_err: scrape errors, [{'url': <failed url>, 'err': <error msg>}]
-            list_parse_err: parsing errors, [{'url': <failed url>, 'err': <error msg>}]
-        '''
-        self.name = name
-        self.parse_func = parse_func
-
-        self.job_queue = queue.Queue()
-        self.lock = threading.Lock()
-        self.concurrent = concurrent
-        self.timeout = timeout
-        self.finished = 0
+        self.job_queue = queue.Queue()  # thread-safe synchronized queue
+        self.lock = threading.Lock()    # Lock for synchronized variable r/w
+        self.concurrent = concurrent    # num. of threads
+        self.timeout = timeout          # timeout for requests
+        self.finished = 0               # tracking scraping progress
 
         self.storage = storage
 
-        self.list_job, self.list_tmp = [], []
-        self.list_data, self.list_scrape_err, self.list_parse_err, self.list_tmp = [], [], [], []
+        self.l_jobs, self.l_tmp = [], []
+        # l_jobs: all urls to scrape
+        # l_tmp: temporarily holds response object for urls in l_jobs
+        self.l_data, self.l_s_errs, self.l_p_errs = [], [], []
 
-        self._spawn_threads()
+        # thread creation
+        self._spawn()
 
+    ''' README
+    Clear the variables
+    '''
     def reset( self ):
-        ''' reset all results, clean for another run '''
         self.finished = 0
-        self.list_job.clear()
-        self.list_data.clear()
-        self.list_scrape_err.clear()
-        self.list_parse_err.clear()
-        self.list_tmp.clear()
+        self.l_jobs.clear()
+        self.l_data.clear()
+        self.l_s_errs.clear()
+        self.l_p_errs.clear()
+        self.l_tmp.clear()
         return self
 
+    ''' README
+    Assign the data input (Type: list).
+    Initiate progress tracing variables.
+    '''
     def input( self, input ):
-        ''' consume url list passed by user '''
-        if( not self.list_job ): self.list_job = input
-        elif( self.list_job ):   self.list_job.extend( input )
+        if( not self.l_jobs ): self.l_jobs = input
+        elif( self.l_jobs ):   self.l_jobs.extend( input )
         return self
 
+    ''' README
+    Assign the name of the task. Simultaneously, the pathes for data commitment are created.
+    '''
     def name_with( self, name ):
         ''' consume name and reconfigure paths '''
         self.name = name
@@ -85,91 +76,109 @@ class scraper():
         self.path_f_parse_err = os.path.join( self.storage, f'{name}_parse_err.json' )
         return self
 
-    def parse_with( self, parse_func ):
-        ''' comsume parsing function passed by user '''
-        self.parse_func = parse_func
+    ''' README
+    Assign the parsing function for scraping.
+    '''
+    def parse_with( self, parse_funct ):
+        self.parse_funct = parse_funct
         return self
 
-    def run_once(self):
-        ''' run once '''
-        pass
+    ''' README
+    Start the process.
+    '''
+    def run( self ):
+        self = self.reset()
+        pre = None
+        while( self.l_jobs or \
+               self.l_s_errs or \
+               self.l_p_errs ):
 
-    def run_until_done(self):
-        ''' run until no list_scrape_err or list_parse_err or stuck '''
-        pre_url_lst_sorted = None
-        while self.list_job or self.list_scrape_err or self.list_parse_err:
-            if( self.list_scrape_err ):
-                self.list_job.extend( list(map(lambda x: x['url'], self.list_scrape_err)) )
+            def create_list( l ): return list(map( lambda x: x[ 'url' ], l ))
 
-            if( self.list_parse_err ):
-                self.list_job.extend( list(map(lambda x: x['url'], self.list_parse_err)) )
+            if( len( self.l_s_errs ) ): self.l_jobs.extend( create_list( self.l_s_errs ) )
+            if( len( self.l_p_errs ) ): self.l_jobs.extend( create_list( self.l_p_errs ) )
 
             # items that keep failed!
-            if( pre_url_lst_sorted and sorted(self.list_job) == pre_url_lst_sorted ):
+            if( pre and sorted(self.l_jobs) == pre ):
                 self._save()
-                report.create_scraper_report( len(self.list_data), len(self.list_scrape_err), len(self.list_parse_err), msg="Some error presented!" )
-                self.list_scrape_err.clear()
-                self.list_parse_err.clear()
+                report.create_scraper_report( len(self.l_data), \
+                                              len(self.l_s_errs), \
+                                              len(self.l_p_errs), msg="Some error presented!" )
+                # self.l_s_errs.clear()
+                # self.l_p_errs.clear()
 
                 return
 
-            pre_url_lst_sorted = sorted( self.list_job )
+            pre = sorted( self.l_jobs )
 
-            self.list_scrape_err.clear()
-            self.list_parse_err.clear()
+            self.l_s_errs.clear()
+            self.l_p_errs.clear()
 
-            print( f'Scraping started with {len(self.list_job)} jobs......' )
-            for url in self.list_job: self.job_queue.put(url)
-
+            print( f'Scraping started with {len(self.l_jobs)} jobs......' )
+            # put the job into job queue
+            for url in self.l_jobs: self.job_queue.put(url)
             self.job_queue.join()
-            self.list_job = []
+
+            self.l_jobs = []
             self._parse()
-            self.list_tmp = []
+            self.l_tmp = []
             self.finished = 0
 
         self._save()
-        report.create_scraper_report( len(self.list_data), len(self.list_scrape_err), len(self.list_parse_err) )
+        report.create_scraper_report( len(self.l_data), \
+                                      len(self.l_s_errs), \
+                                      len(self.l_p_errs) )
 
+    ''' README
+    Commit the results.
+    '''
     def _save( self ):
-        ''' save final data. list_data, list_scrape_err, list_parse_err '''
-        write_to_json( self.path_f_data, self.list_data )
-        write_to_json( self.path_f_scrape_err, self.list_scrape_err )
-        write_to_json( self.path_f_parse_err, self.list_parse_err )
+        ''' save final data. l_data, l_s_errs, l_p_errs '''
+        write_to_json( self.path_f_data, self.l_data )
+        write_to_json( self.path_f_scrape_err, self.l_s_errs )
+        write_to_json( self.path_f_parse_err, self.l_p_errs )
 
+    ''' README
+    Assign the job for each thread.
+    '''
     def _job( self ):
-        ''' job for each thread, makes list_tmp '''
+        ''' job for each thread, makes l_tmp '''
         while( True ):
             url = self.job_queue.get()
             try:
                 res = requests.get( url, timeout=self.timeout )
                 self.lock.acquire()
-                self.list_tmp.append(res)
+                self.l_tmp.append(res)
             except Exception as err:
                 self.lock.acquire()
-                self.list_scrape_err.append( { 'url': url, 'err': str(err) } )
+                self.l_s_errs.append( { 'url': url, 'err': str(err) } )
             finally:
                 self.finished += 1
-                print(f'Progress: {100 * self.finished / len(self.list_job):.2f}%', end='\r')
+                print( f'Progress: {100 * self.finished / len(self.l_jobs):.2f}%', end='\r' )
                 self.lock.release()
                 self.job_queue.task_done()
 
-    def _spawn_threads( self ):
-        ''' start threads as daemons '''
+    ''' README
+    Create the threads.
+    '''
+    def _spawn( self ):
         for _ in range(0, self.concurrent):
             thread = threading.Thread(target=self._job)
             thread.daemon = True
             thread.start()
 
+    ''' README
+    Parsing the web source using given parsing funct.
+    '''
     def _parse( self ):
-        ''' parse res using parse_func '''
         i = 0
         print( '\n' )
-        for res in self.list_tmp:
+        for res in self.l_tmp:
             try:
-                data = self.parse_func(res)
-                self.list_data.extend(data)
+                data = self.parse_funct(res)
+                self.l_data.extend(data)
             except Exception as err:
-                self.list_parse_err.append({'url': res.url, 'err': repr(err), 'trace': format_exc() })
+                self.l_p_errs.append({'url': res.url, 'err': repr(err), 'trace': format_exc() })
             i += 1
             print( f'Parsing..... {i}', end='\r' )
         print( '\nParsing completed!\n' )
@@ -179,8 +188,14 @@ class scraper():
 Trigger the scraper class and perform action
 
 Input:
+  - name: name of the task
   - in_chunk: whether to perform actions in parts, True/False
-
+  - data: data input
+  - parse_funct: parsing funct for scraping
+  - start: index for job starting point
+  - concurrent: num. of threads
+  - partition: size of chunk
+  - timeout: timeout for reqests
 '''
 def trigger_scraper( name='scrape', in_chunk=False,\
     data=[], parse_funct=(lambda x: x.text),
@@ -201,10 +216,10 @@ def trigger_scraper( name='scrape', in_chunk=False,\
             tail = (i + partition)
             if( tail >= len( data ) ): tail = len( data )
 
-            s.reset().name_with( '{}_{}-{}'.format( name, i, tail ) )
-            s.input( data[ i:tail ] ).parse_with( parse_funct ).run_until_done()
+            s.name_with( '{}_{}-{}'.format( name, i, tail ) )
+            s.input( data[ i:tail ] ).parse_with( parse_funct ).run()
     # run in whole
-    else: s.reset().name_with( name ).input( data ).parse_with( parse_funct ).run_until_done()
+    else: s.name_with( name ).input( data ).parse_with( parse_funct ).run()
 
 if __name__ == '__main__':
     pass
