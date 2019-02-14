f83e3ae6dbac7a5338a276d5d8c511375d1eaaa0
hippoandy <hippo1993928@gmail.com>
2019-02-14 20:57:45 -0300

    Debugged and update version num

--- utilsDAWS/ops_scrape/clean.py
+++ utilsDAWS/ops_scrape/clean.py
@@ -8,7 +8,7 @@ Author: Yu-Chang Ho
 from utilsDAWS import config
 from utilsDAWS import ops_data as ops
 from utilsDAWS import ops_file as rw
-from utilsDAWS.ops_thread import requester
+from utilsDAWS.ops_request import requester
 
 import glob, os, time
 import textwrap
