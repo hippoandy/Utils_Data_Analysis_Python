505d12ed6da7676520092a5bef32209b8d5c2f1c
hippoandy <ycaho@ucdavis.edu>
2019-02-14 14:11:52 -0300

    Add comments and update STDOUT msg

--- utilsDAWS/ops_report/report.py
+++ utilsDAWS/ops_report/report.py
@@ -9,6 +9,7 @@ __all__ = [
 class reporter():
     def __init__( self ):
         self.t_start = time.time()
+        self.p_start = None        # record start poing
 
     ''' README
 
@@ -20,13 +21,31 @@ class reporter():
       - t_start: program start time
     '''
     def create_progress_report( self, num_job, current ):
+        if( self.p_start == None ): self.p_start = current
+        total = (num_job - current)
+        completed = (current - self.p_start)
+
         print( textwrap.dedent( f'''\
+            ----------------------------------------------------------
             Status Report:
-                Remaining jobs: {num_job - current}
-                Percentage:     {100 * current / num_job:.2f} %
-                Executed for    {time.time() - self.t_start} seconds
+                Total jobs:     {total}
+                Completed jobs: {completed}
+                Remaining jobs: {total - completed}
+                Percentage:     {100 * completed / total:.2f} (%)
+                Executed for    {time.time() - self.t_start:.2f} (sec)
+            ----------------------------------------------------------
         ''' ) )
 
+''' README
+
+Create status report for scraper class.
+
+Input:
+    - len_data: total num. of data saved
+    - len_scrape_err: num. of job failed due to scraping error
+    - len_parse_err:  num. of job failed due to parsing error
+    - msg: custom message
+'''
 def create_scraper_report( len_data, len_scrape_err, len_parse_err, msg="Finished" ):
     print(textwrap.dedent(f'''\
         {msg}:
