7d107ec1e9be5cd90f1965e1d276eb392ab56d44
hippoandy <ycaho@ucdavis.edu>
2019-02-18 12:50:45 -0300

    Add file download funct.

---
+++ utilsDAWS/file/download.py
@@ -0,0 +1,22 @@
+from utilsDAWS import logger
+from utilsDAWS import file
+
+import glob
+import urllib.request
+
+__all__ = [
+    'download'
+]
+
+# function to download the files
+def download( url, f_storage, f_name, f_type ):
+    # create the logger
+    l = logger( fname=r'dl_failed' )
+
+    f_path = r'{}/{}'.format( f_storage, r'{}.{}'.format( f_name, f_type ) )
+    if( file.is_file_exist( f_path ) ):
+        num = len( glob.glob( r'{}/{}'.format( f_storage, r'{}*'.format( f_name ) ) ) )
+        f_path = r'{}/{}'.format( f_storage, r'{}_({}).{}'.format( f_name, (num + 1), f_type ) )
+
+    try: urllib.request.urlretrieve( url, f_path )
+    except: l.commit( type='error', msg=f'Failed to download: {url}' )
