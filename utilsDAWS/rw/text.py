e17cee55a08d7a27399a406c58b895e7e3cc1379
hippoandy <ycaho@ucdavs.edu>
2019-02-19 13:21:56 -0300

    Updated

--- utilsDAWS/rw/text.py
+++ utilsDAWS/rw/text.py
@@ -1,17 +1,51 @@
+import glob
+
+import sys
+sys.path.append( '..' )
+import config
 
 __all__ = [
     'write_to_text',
-    'write_to_log_text'
+    'write_to_log_text',
+    'concat_text_files'
 ]
 
+'''README
+
+Write plain text data into a file.
+
+Return: data file commitment
+'''
 def write_to_text( path, data, encode='utf-8' ):
     f = open( path, 'w+', encoding=encode )
     f.write( data )
     f.close()
 
-# write log in plain text to a file
+'''README
+
+Write log in plain text format to a file.
+
+Return: data file commitment
+'''
 def write_to_log_text( path, data, encode='utf-8' ):
     write_to_text( path, data, encode=encode )
+
+'''README
+
+Combine multiple text files into one.
+
+Return: data file commitment
+'''
+def concat_text_files( dir_files=config.path_data, files=config.f_data_txt, \
+    dir_result=config.path_data, result=config.f_concated_txt, \
+    encode=config.encoding_f, \
+    del_origin=False, del_empty=False ):
+
+    with open( r'{}/{}'.format( dir_result, result ), 'r', encoding=encode ) as result:
+        for n in glob.glob( r'{}/{}'.format( dir_files, files ) ):
+            f = open( n, 'r', encoding=encode )
+            for l in f.readlines(): result.write( l + '\n' )
+    result.close()
 
 if __name__ == '__main__':
     pass
