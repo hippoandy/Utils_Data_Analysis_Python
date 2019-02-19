40f886489e21c1e309b2e7702fed1f4e411d3a81
hippoandy <ycaho@ucdavs.edu>
2019-02-19 11:02:33 -0300

    Add new rw funct

--- utilsDAWS/rw/text.py
+++ utilsDAWS/rw/text.py
@@ -1,13 +1,17 @@
 
 __all__ = [
+    'write_to_text',
     'write_to_log_text'
 ]
 
-# write log in plain text to a file
-def write_to_log_text( path, data, encode='utf-8' ):
+def write_to_text( path, data, encode='utf-8' ):
     f = open( path, 'w+', encoding=encode )
     f.write( data )
     f.close()
+
+# write log in plain text to a file
+def write_to_log_text( path, data, encode='utf-8' ):
+    write_to_text( path, data, encode=encode )
 
 if __name__ == '__main__':
     pass
